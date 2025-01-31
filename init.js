dd8fe37b519cbfe8b00158173f63aeca18535f2d
Dim0509-hab <pripoy.kanifol@gmail.com>
2025-01-31 19:32:49 +0500

    + Кнопки *Пуск* и *Сброс*

--- init.js
+++ init.js
@@ -1,6 +1,5 @@
    
-   
-window.onload = function()
+document.getElementById('btnRetry').addEventListener('click', function ()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
@@ -9,13 +8,11 @@ window.onload = function()
     document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
     document.getElementById('birthDayOutput').innerText = initPerson.birthDay + " " + initPerson.monthDay;
     document.getElementById('professionOutput').innerText = initPerson.profession;
-    document.getElementById('patronymicOutput').innerText = initPerson.patronymic;     
+    document.getElementById('patronymicOutput').innerText = initPerson.patronymic; 
 
-};
+});  
 
-document.getElementById('btnRetry').addEventListener('click', function () {  //  Пуск
-    window.location.reload(window.onload)
-   })
-
-
-
+document.getElementById('btnClear').addEventListener('click', function () {
+    
+    window.location.reload();    
+})
